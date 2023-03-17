c7ea3cda5984cb81068d7b408ae8e6cbf6f838d2
XPS17Ubuntu <eirikmathias@gmail.com>
2023-03-17 20:24:28 +0100

    Ferdig oving 8

--- Cpp_TDT4102/08Oving/Matrix.cpp
+++ Cpp_TDT4102/08Oving/Matrix.cpp
@@ -99,15 +99,15 @@ Matrix& Matrix::operator=(Matrix& mat){
 			listPtrCol[i][k] = mat.get(i,k);
 		}
 	}
-	return mat
+	return *this;
 }
 
 Matrix& Matrix::operator+=(Matrix& mat){
 	for (int i=0; i < ncolums; i++){
 		for (int k= 0; k < nrows; k++){
 			double val = listPtrCol[i][k] + mat.get(k,i);
-			mat.set(k,i,val);
+			listPtrCol[i][k] = val;
 		}
 	}
-	return mat;
+	return *this;
 }
--- Cpp_TDT4102/08Oving/main.cpp
+++ Cpp_TDT4102/08Oving/main.cpp
@@ -9,18 +9,18 @@ int main(){
 	std::cout <<"Start of program"<<std::endl;
 	Matrix A(3,3);
 	Matrix B(3,3);
-	Matrix C(2,3);
+	Matrix C(3,3);
 	for (int i = 0; i < 3; i++){
 		for (int k = 0; k < 3; k++){
-			A.set(i,k, 4+i);
-			B.set(k,i, 3-1);
+			A.set(i,k, 4+k);
+			B.set(k,i, 3-i);
 		}
 		
 	}
 	C += A;
 	C += B;
 
-	std::cout << A << C << std::endl;
+	std::cout << A << B << C << std::endl;
 
 
 	dummyTest();
