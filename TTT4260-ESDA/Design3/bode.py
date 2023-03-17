5c1fea6309f6356e35a60301342af6e33cfab8a5
XPS17Ubuntu <eirikmathias@gmail.com>
2023-03-17 14:22:22 +0100

    added new data

---
+++ TTT4260-ESDA/Design3/bode.py
@@ -0,0 +1,59 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# plot bode plot for a rcl circuit from csv file
+
+Frequency = []
+C1_gain = []
+C2_gain = []
+phase = []
+
+# read csv file
+with open('bode1K.csv', 'r') as f:
+	data = f.readlines()
+	# remove header
+	header = data[0]
+	data = data[1:]
+	for line in data:
+		# split line
+		split_line = line.split(',')
+		# append to lists
+		Frequency.append(float(split_line[0]))
+		C1_gain.append(float(split_line[1]))
+		C2_gain.append(float(split_line[2]))
+		phase.append(float(split_line[3]))
+
+#find index and value of max gain
+max_gain = min(C2_gain)
+max_gain_index = C2_gain.index(max_gain)
+max_gain_freq = round(Frequency[max_gain_index], 0)
+
+#find gain value of 2489Hz
+for i in range(len(Frequency)):
+	if Frequency[i] >= 2489:
+		gain_2489 = C2_gain[i]
+		index = i
+		actual = (C2_gain[i] + C2_gain[i-1])/2
+		print('Gain at 2489 Hz: ' + str(actual) + ' dB')
+		break
+
+
+#plot
+plt.figure(1)
+plt.subplot(211)
+plt.semilogx(Frequency, C1_gain, label='C1')
+plt.semilogx(Frequency, C2_gain, label='C2')
+plt.axvline(2489, color='g', label='2489 Hz' + ', ' + str(round(actual,1)) + ' dB')
+plt.axvline(max_gain_freq, color='r', linestyle='dotted', label=str(max_gain_freq) + ' Hz' + ', ' + str(round(max_gain,1)) + ' dB')
+plt.legend()
+plt.ylabel('Gain (dB)')
+plt.subplot(212)
+plt.semilogx(Frequency, phase)
+plt.xlabel('Frequency (Hz)')
+plt.ylabel('Phase (deg)')
+plt.savefig('./LaTeX/Bilder/bode1K.png', dpi=600)
+#plt.show()
+
+# Save plot
+
+
---
+++ TTT4260-ESDA/Design3/spectrum.py
@@ -0,0 +1,51 @@
+import matplotlib.pyplot as plt
+
+
+## Plot the spectrum analysis of another signal from csv file
+#spectrumReadFile = 'spectrum2K.csv'
+#spectrumWriteFile = './LaTeX/Bilder/spectrum2K.png'
+
+spectrumReadFile = 'spectrum2x1K.csv'
+spectrumWriteFile = './LaTeX/Bilder/spectrum2x1K.png'
+
+Frequency = []
+C1_gain = []
+C2_gain = []
+C1_phase = []
+C2_phase = []
+
+# read csv file
+with open(spectrumReadFile, 'r') as f:
+	data = f.readlines()
+	# remove header
+	header = data[0]
+	data = data[1:]
+	for line in data:
+		# split line
+		split_line = line.split(',')
+		# append to lists
+		Frequency.append(float(split_line[0]))
+		C1_gain.append(float(split_line[1]))
+		C2_gain.append(float(split_line[3]))
+		C1_phase.append(float(split_line[2]))
+		C2_phase.append(float(split_line[4]))
+
+#find index and value of max gain
+max_gain = max(C1_gain)
+max_gain_index = C1_gain.index(max_gain)
+max_gain_freq = round(Frequency[max_gain_index], 0)
+
+#find gain value after reduction
+red_gain = C2_gain[max_gain_index]
+
+#Plot the spectrum analysis without phase
+
+plt.semilogx(Frequency, C1_gain, label='C1')
+plt.semilogx(Frequency, C2_gain, label='C2')
+plt.semilogx(max_gain_freq, max_gain, 'ro', label=str(max_gain_freq) + ' Hz' + ', ' + str(round(max_gain,1)) + ' dB')
+plt.semilogx(max_gain_freq, red_gain, 'go', label='2489 Hz' + ', ' + str(round(red_gain,1)) + ' dB')
+plt.legend()
+plt.xlabel('Frequency (Hz)')
+plt.ylabel('Gain (dB)')
+plt.savefig(spectrumWriteFile, dpi=600)
+#plt.show()
