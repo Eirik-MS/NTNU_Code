697708155a1511ed3a14ad8964fec4c7493d8267
Xprice5804 <eirikmathias@gmail.com>
2023-10-22 19:47:39 +0200

    Stuff

--- TTT4260-ESDA/Design5/ITGK.py
+++ TTT4260-ESDA/Design5/ITGK.py
@@ -11,5 +11,21 @@ def geo_rekke(utrykk, toleranse):
         n += 1
     return summ
 
-print(geo_rekke(0.5, 0.00001))
+#print(geo_rekke(0.5, 0.00001))
 
+
+
+##%%%###
+import math 
+
+def pi(d = 2):
+    if d > 15:
+        return "Error: d must be less than 15"
+    else:
+        return round(math.pi, d)
+    
+
+print(pi(16))
+print(pi(15))
+print(pi(4))
+print(pi())
